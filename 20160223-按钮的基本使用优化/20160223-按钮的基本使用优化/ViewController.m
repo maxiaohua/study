8ee8c88725033959d7974b4e8ea74ab90cda3ebb
心忆年华 <mazhihua5152570@126.com>
2016-02-23 21:49:44 +0800

    button变大变小

--- 20160223-按钮的基本使用优化/20160223-按钮的基本使用优化/ViewController.m
+++ 20160223-按钮的基本使用优化/20160223-按钮的基本使用优化/ViewController.m
@@ -24,6 +24,27 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+- (IBAction)bigOrSmall:(UIButton *)sender {
+    CGRect frameTemp = self.Icon.frame;
+    switch (sender.tag) {
+        case 100:
+            frameTemp.size.width += 20;
+            frameTemp.size.height += 20;
+            break;
+            
+        case 200:
+            frameTemp.size.width -= 20;
+            frameTemp.size.height -= 20;
+            break;
+  
+        default:
+            break;
+    }
+    
+    //设置button变大变小
+    self.Icon.frame = frameTemp;
+    
+}
 - (IBAction)buttonClick:(UIButton *)sender {
     CGPoint IconCenter = self.Icon.center;
     switch (sender.tag) {
