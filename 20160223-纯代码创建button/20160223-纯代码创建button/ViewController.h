576e05d24ce1cdd5497510ce9285ffdb617c7c2e
心忆年华 <mazhihua5152570@126.com>
2016-02-23 20:37:45 +0800

    创建项目

---
+++ 20160223-纯代码创建button/20160223-纯代码创建button/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  20160223-纯代码创建button
+//
+//  Created by mazhi'hua on 16/2/23.
+//  Copyright © 2016年 mazhi'hua. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ 20160223-纯代码创建button/20160223-纯代码创建button/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  20160223-纯代码创建button
+//
+//  Created by mazhi'hua on 16/2/23.
+//  Copyright © 2016年 mazhi'hua. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
