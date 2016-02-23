408f612b783b051b7eb1163e65b96559aee0ca44
心忆年华 <mazhihua5152570@126.com>
2016-02-23 22:21:07 +0800

    新建项目 QQ聊天界面  完成界面搭建部分

---
+++ 20160223-QQ登陆界面/20160223-QQ登陆界面/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  20160223-QQ登陆界面
+//
+//  Created by mazhi'hua on 16/2/23.
+//  Copyright © 2016年 mazhi'hua. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
