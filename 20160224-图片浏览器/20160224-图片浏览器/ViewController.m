677b84e4f3b242fc5b8f91e399374495095dd2ae
心忆年华 <mazhihua5152570@126.com>
2016-02-24 22:13:59 +0800

    创建图片浏览器项目

---
+++ 20160224-图片浏览器/20160224-图片浏览器/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  20160224-图片浏览器
+//
+//  Created by mazhi'hua on 16/2/24.
+//  Copyright © 2016年 mazhi'hua. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ 20160224-图片浏览器/20160224-图片浏览器/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  20160224-图片浏览器
+//
+//  Created by mazhi'hua on 16/2/24.
+//  Copyright © 2016年 mazhi'hua. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
